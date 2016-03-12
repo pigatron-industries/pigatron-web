b395dd55c2daaff65a2c7d522d4dd9b30e3426ee
roberte <rob@pigatron-industries.co.uk>
2016-03-12 07:43:39 +0000

    Configured web resource optimizer

---
+++ web-admin/src/main/java/com/pigatron/shop/config/Wro4jCustomXmlModelManagerFactory.java
@@ -0,0 +1,56 @@
+package com.pigatron.shop.config;
+
+import ro.isdc.wro.manager.factory.ConfigurableWroManagerFactory;
+import ro.isdc.wro.model.factory.WroModelFactory;
+import ro.isdc.wro.model.factory.XmlModelFactory;
+import ro.isdc.wro.model.resource.processor.factory.ProcessorsFactory;
+import ro.isdc.wro.model.resource.processor.factory.SimpleProcessorsFactory;
+import ro.isdc.wro.model.resource.processor.impl.css.CssUrlRewritingProcessor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Wro4jCustomXmlModelManagerFactory extends ConfigurableWroManagerFactory {
+//	private static final Logger log = LoggerFactory
+//			.getLogger(Wro4jCustomXmlModelManagerFactory.class);
+
+	final private Properties props;
+
+	public Wro4jCustomXmlModelManagerFactory(Properties props) {
+		this.props = props;
+	}
+
+	@Override
+	protected Properties newConfigProperties() {
+		return props;
+	}
+
+	@Override
+	protected WroModelFactory newModelFactory() {
+		//log.debug("loading from /wro.xml");
+		return new XmlModelFactory() {
+			@Override
+			protected InputStream getModelResourceAsStream() throws IOException {
+				String resourceLocation = "/wro.xml";
+				//log.info("Loading resource {}", resourceLocation);
+				final InputStream stream = getClass().getResourceAsStream(
+						resourceLocation);
+
+				if (stream == null) {
+					throw new IOException("Invalid resource requested: "
+							+ resourceLocation);
+				}
+
+				return stream;
+			}
+		};
+	}
+
+	@Override
+	protected ProcessorsFactory newProcessorsFactory() {
+		final SimpleProcessorsFactory rv = new SimpleProcessorsFactory();
+		rv.addPreProcessor(new CssUrlRewritingProcessor());
+		return rv;
+	}
+}
--- web-admin/src/main/java/com/pigatron/shop/web/admin/AdminController.java
+++ web-admin/src/main/java/com/pigatron/shop/web/admin/AdminController.java
@@ -5,18 +5,24 @@ import com.pigatron.shop.service.InitialConfigurationService;
 import com.pigatron.shop.web.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.authentication.rememberme.AbstractRememberMeServices;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
+import org.webjars.WebJarAssetLocator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class AdminController {
@@ -83,6 +89,19 @@ public class AdminController {
 		SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
 		cookieClearingLogoutHandler.logout(request, response, null);
 		securityContextLogoutHandler.logout(request, response, null);
+	}
+
+	@RequestMapping("/lib/{webjar}/**")
+	public ResponseEntity locateWebjarAsset(@PathVariable String webjar, HttpServletRequest request) {
+		try {
+			WebJarAssetLocator webJarAssetLocator = new WebJarAssetLocator();
+			String mvcPrefix = "/lib/" + webjar + "/"; // This prefix must match the mapping path!
+			String mvcPath = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+			String fullPath = webJarAssetLocator.getFullPath(webjar, mvcPath.substring(mvcPrefix.length()));
+			return new ResponseEntity(new ClassPathResource(fullPath), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
