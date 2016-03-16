563d2cb3bba42779c1a1076bfce2829f5a611cd2
roberte <rob@pigatron-industries.co.uk>
2016-03-16 12:51:42 +0000

    Added maven plugin

--- src/test/java/com/pigatron/ApplicationTests.java
+++ src/test/java/com/pigatron/ApplicationTests.java
@@ -1,12 +1,7 @@
 package com.pigatron;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
 
 //	@Test
