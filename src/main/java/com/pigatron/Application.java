8a3fa92c25d0260dfe72555fca0e4b1233db4136
Robert Ellis <rob@pigatron-industries.co.uk>
2016-06-01 21:48:02 +0100

    changed location of build info property file

--- src/main/java/com/pigatron/Application.java
+++ src/main/java/com/pigatron/Application.java
@@ -3,9 +3,11 @@ package com.pigatron;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.AbstractApplicationContext;
 
 @SpringBootApplication
+@PropertySource("classpath:/default.properties")
 public class Application {
 
 	static ApplicationContext context;
