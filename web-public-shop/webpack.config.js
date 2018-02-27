af1a299ac257717b2d5c079b8de7ffeaf0244ed0
rob <rob@pigatron-industries.co.uk>
2018-02-27 17:47:14 +0000

    Upgrade to webpack 2

--- web-public-shop/webpack.config.js
+++ web-public-shop/webpack.config.js
@@ -1,78 +1,11 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var failPlugin = require('webpack-fail-plugin');
-
-var moduleName = "shop";
-var namespace = "pigatron";
-
-var paths = {
-    src: './src/main/resources/static',
-    dst: './build/resources/main/static'
-};
-
-var files = {
-    tsMain: paths.src+'/public/'+moduleName+'/main.ts',
-    //tsIgnore: paths.src+'/public/'+moduleName+'/ignore.ts',
-    jsOutput: paths.dst+'/public/[name].js'
-};
-
-module.exports = {
-    entry: {
-        'public_shop': files.tsMain
-    },
-    output: {
-        path: paths.dst+'/public/',
-        filename: '[name].js',
-        publicPath: '/public/',
-        library: [namespace, '[name]'],
-        libraryTarget: 'var'
-    },
-    resolve: {
-        extensions: ['', '.js', '.ts']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'ts'
-            },
-            {
-                test: /\.html$/,
-                loader: 'html'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
-            },
-            {
-                test: /\.css$/,
-                exclude: paths.src,
-                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-            },
-            {
-                test: /\.css$/,
-                include: paths.src,
-                loader: 'raw'
-            }
-        ]
-    },
-    plugins: [
-        failPlugin,
-        new ExtractTextPlugin('[name].css'),
-        //new webpack.optimize.DedupePlugin(),
-        //new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['public_shop']
-        })
-    ],
-    externals:[
-        function(context, request, callback) {
-            if(request.indexOf("@angular") != -1 ||
-                request.indexOf("rxjs") != -1)
-                return callback(null, "var window.pigatron.public_lib");
-            else if(request.indexOf("pigatron/public/core") != -1)
-                return callback(null, "var window.pigatron.public_core");
-            callback();
-        }
-    ]
-};
+var createConfig = require('../webpack.common.config.js');
+module.exports = createConfig(__dirname, "public", "shop", false, false,
+    function(context, request, callback) {
+        if(request.indexOf("@angular") != -1 ||
+            request.indexOf("rxjs") != -1)
+            return callback(null, "var window.pigatron.public_lib");
+        else if(request.indexOf("pigatron/public/core") != -1)
+            return callback(null, "var window.pigatron.public_core");
+        callback();
+    }
+);
---
+++ webpack.common.config.js
@@ -0,0 +1,83 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var failPlugin = require('webpack-fail-plugin');
+var path = require('path');
+
+module.exports = function(dirname, appName, moduleName, vendor, polyfills, externals) {
+
+    var paths = {
+        src: path.resolve(dirname, './src/main/resources/static'),
+        dst: path.resolve(dirname, './build/resources/main/static')
+    };
+
+    var files = {
+        tsMain: paths.src+'/'+appName+'/'+moduleName+'/main.ts',
+        tsVendor: paths.src+'/'+appName+'/'+moduleName+'/lib.ts',
+        tsPolyfills: paths.src+'/'+appName+'/'+moduleName+'/polyfill.ts',
+        jsOutput: paths.dst+'/'+appName+'/[name].js'
+    };
+
+    var entry = {};
+    entry[appName+'_'+moduleName] = files.tsMain;
+    if(polyfills)
+        entry['polyfill'] = files.tsPolyfills;
+    if(vendor)
+        entry[appName+'_lib'] = files.tsVendor;
+
+    var names = [];
+    names.push(appName+'_'+moduleName);
+    if(polyfills)
+        names.push('polyfill');
+    if(vendor)
+        names.push(appName+'_lib');
+
+    return {
+        entry: entry,
+        output: {
+            path: paths.dst+'/'+appName+'/',
+            filename: '[name].js',
+            publicPath: '/'+appName+'/',
+            library: ['pigatron', '[name]'],
+            libraryTarget: 'var'
+        },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader'
+                },
+                {
+                    test: /\.html$/,
+                    use: 'html-loader'
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: 'file-loader?name=assets/[name].[hash].[ext]'
+                },
+                {
+                    test: /\.css$/,
+                    exclude: paths.src,
+                    use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+                },
+                {
+                    test: /\.css$/,
+                    include: paths.src,
+                    use: 'raw-loader'
+                }
+            ]
+        },
+        plugins: [
+            failPlugin,
+            new ExtractTextPlugin('[name].css'),
+            //new webpack.optimize.DedupePlugin(),
+            //new webpack.optimize.UglifyJsPlugin(),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: names
+            })
+        ],
+        externals: externals
+    };
+};
