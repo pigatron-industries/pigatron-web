142fd9edbc30b4389c3590d462d8bc4f7991b984
Robert Ellis <rob@pigatron-industries.co.uk>
2018-03-01 21:11:16 +0000

    Updated admin core to use scss

--- web-admin-core/src/main/resources/static/admin/core/main.ts
+++ web-admin-core/src/main/resources/static/admin/core/main.ts
@@ -1,4 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AdminAppModule } from './app/app.module';
+import './style/main.scss';
 
 platformBrowserDynamic().bootstrapModule(AdminAppModule);
--- web-public-core/src/main/resources/static/public/core/main.ts
+++ web-public-core/src/main/resources/static/public/core/main.ts
@@ -1,5 +1,5 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {PublicAppModule} from './app/app.module';
-import './style/web-public-core.scss';
+import './style/main.scss';
 
 platformBrowserDynamic().bootstrapModule(PublicAppModule);
