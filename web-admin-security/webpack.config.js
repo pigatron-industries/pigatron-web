5637cf0a211116de5edc2b9c386be5c452ba4f5a
rob <rob@pigatron-industries.co.uk>
2018-03-17 18:38:47 +0000

    fixed webpack config for admin-security module

--- web-admin-security/webpack.config.js
+++ web-admin-security/webpack.config.js
@@ -3,9 +3,9 @@ module.exports = createConfig(__dirname, "admin", "security", false, false,
     function(context, request, callback) {
         if(request.indexOf("@angular") != -1 ||
             request.indexOf("rxjs") != -1)
-            return callback(null, "var window.pigatron.public_lib");
+            return callback(null, "var window.pigatron.admin_lib");
         else if(request.indexOf("pigatron/admin/core") != -1)
-            return callback(null, "var window.pigatron.public_core");
+            return callback(null, "var window.pigatron.admin_core");
         callback();
     }
 );
