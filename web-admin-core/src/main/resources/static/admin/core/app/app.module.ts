4d0d6028a577c67596b933315843f3d1258cbb60
Robert Ellis <rob@pigatron-industries.co.uk>
2018-03-18 15:14:26 +0000

    use material flex-layout

--- web-admin-core/src/main/resources/static/admin/core/app/app.module.ts
+++ web-admin-core/src/main/resources/static/admin/core/app/app.module.ts
@@ -2,17 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 import {AdminAppComponent} from './app.component';
 import {HomeComponent} from './home.component';
-//import { HomeLeftComponent } from './home.left.component';
 import {routes} from './app.routes';
 
 @NgModule({
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        RouterModule.forRoot(routes, { enableTracing: true })
+        RouterModule.forRoot(routes, { enableTracing: true }),
+        FlexLayoutModule
     ],
     declarations: [ AdminAppComponent, HomeComponent ],
     bootstrap:    [ AdminAppComponent ]
--- web-public-core/src/main/resources/static/public/core/app/app.module.ts
+++ web-public-core/src/main/resources/static/public/core/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
-import { routes } from './app.routes';
-import { PublicAppComponent }   from './app.component';
-import { HomeComponent } from './home.component';
+import {routes} from './app.routes';
+import {PublicAppComponent}   from './app.component';
+import {HomeComponent} from './home.component';
 
 
 @NgModule({
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        RouterModule.forRoot(routes, { enableTracing: true })
+        RouterModule.forRoot(routes, { enableTracing: true }),
+        FlexLayoutModule
     ],
     declarations: [ PublicAppComponent, HomeComponent ],
     bootstrap:    [ PublicAppComponent ]
