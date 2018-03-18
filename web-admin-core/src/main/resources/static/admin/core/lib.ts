5d27269cf192e81d4dd563fe1b6e90ce0661b0d0
Robert Ellis <rob@pigatron-industries.co.uk>
2018-03-18 15:22:07 +0000

    use material flex-layout

--- web-admin-core/src/main/resources/static/admin/core/lib.ts
+++ web-admin-core/src/main/resources/static/admin/core/lib.ts
@@ -5,6 +5,7 @@ import "@angular/core";
 import "@angular/common";
 import "@angular/http";
 import "@angular/router";
+import "@angular/flex-layout";
 import "rxjs";
 
 // RxJS
@@ -16,7 +17,8 @@ import "@angular/material";
 //require("font-awesome/css/font-awesome.css");
 
 export * from "@angular/core";
-export { Routes, RouterModule, Router } from '@angular/router';
-export { CommonModule } from '@angular/common';
+export {Routes, RouterModule, Router} from '@angular/router';
+export {CommonModule} from '@angular/common';
+export {FlexLayoutModule} from "@angular/flex-layout";
 //export * from "@angular/http";
 //export * from "rxjs/Observable";
--- web-admin-security/src/main/resources/static/admin/security/login/login.module.ts
+++ web-admin-security/src/main/resources/static/admin/security/login/login.module.ts
@@ -1,17 +1,17 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule } from '@angular/common';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import {NgModule}      from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
-import { LoginComponent }   from './login.component';
-import { routes } from './login.route';
+import {LoginComponent}   from './login.component';
+import {routes} from './login.route';
 
 
 @NgModule({
     imports: [
         CommonModule,
-        //BrowserAnimationsModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        FlexLayoutModule
     ],
     declarations: [ LoginComponent ],
     bootstrap:    [ LoginComponent ]
