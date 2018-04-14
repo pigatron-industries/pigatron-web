4796a07205cd9abed4f6f6e8668f66fa97a475ff
Robert Ellis <rob@pigatron-industries.co.uk>
2018-04-14 18:56:03 +0100

    Use MongoRepository instead of Repository

--- web-server-core/src/main/java/com/pigatron/web/core/repository/BaseRepository.java
+++ web-server-core/src/main/java/com/pigatron/web/core/repository/BaseRepository.java
@@ -1,16 +1,8 @@
 package com.pigatron.web.core.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @NoRepositoryBean
-public interface BaseRepository<T> extends Repository<T, String> {
-    Optional<T> findById(String id);
-    List<T> findAll();
-    <S extends T> S save(S entity);
-    void deleteAll();
-    void deleteById(String id);
+public interface BaseRepository<T> extends MongoRepository<T, String> {
 }
