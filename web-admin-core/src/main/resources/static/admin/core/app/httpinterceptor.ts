91bf0a18058db1374358163fcbb0330f13779211
Robert Ellis <rob@pigatron-industries.co.uk>
2018-04-14 12:50:06 +0100

    Foreward to login page if unauthenticated

--- web-admin-core/src/main/resources/static/admin/core/app/httpinterceptor.ts
+++ web-admin-core/src/main/resources/static/admin/core/app/httpinterceptor.ts
@@ -1,14 +1,28 @@
-import {Injectable} from '@angular/core';
-import {HttpInterceptor, HttpRequest, HttpHandler} from "@angular/common/http";
+import {Injectable, Inject} from '@angular/core';
+import {Router} from '@angular/router'
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse} from "@angular/common/http";
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class XhrInterceptor implements HttpInterceptor {
 
+    constructor(@Inject(Router) private router: Router) {
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const xhr = req.clone({
+        const request = req.clone({
             // prevent browser from showing http basic auth popup
             headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
         });
-        return next.handle(xhr);
+        return next.handle(request).do(
+            () => {},
+            (err: any) => {
+                if (err instanceof HttpErrorResponse) {
+                    // forward to login page if unauthenticated
+                    if(err.status == 401 || err.status == 403) {
+                        this.router.navigateByUrl("login");
+                    }
+                }
+            });
     }
 }
